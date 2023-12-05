b7e16dcbd5d1af63b0cc7ac234d4554f96ef1b9f
Zaydee <zaydominguez8223@gmail.com>
2023-12-04 23:09:03 -0500

    deleting files - cleanup

--- App.vue
+++ App.vue
@@ -1,12 +1,25 @@
+<!-- App vue -->
+
 <script setup>
+// import from vue router
 import { RouterLink, RouterView } from 'vue-router'
+
+//import the navigation header - only place that has this!
 import NavigationHeader from './components/NavigationHeader.vue'
 </script>
 
+
+<!-- begin template -->
 <template>
+
+  <!-- should only have nav + router -->
   <header>
+
+    <!-- nav header -->
       <NavigationHeader></NavigationHeader>
   </header>
+
+  <!-- router -->
   <RouterView />
 </template>
 
@@ -150,7 +163,7 @@ sessions: [
 </script>
 
 
-<!-- begin styling -->
+<!-- begin styling, if needed -->
 <style scoped>
 
 </style>
