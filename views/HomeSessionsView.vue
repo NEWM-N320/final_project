bb1bf06022fc08e5d17e52191cec36f2cfc09fb6
Zaydee <zaydominguez8223@gmail.com>
2023-12-06 01:12:39 -0500

    adding filtering functionality

--- views/HomeSessionsView.vue
+++ views/HomeSessionsView.vue
@@ -7,10 +7,11 @@
        <!-- filler text - display the num of sessions -->
     <p class="ses-count">Session count here</p>
     <!-- slotted btn to clear filter -->
-    <FilterSlottedButton>Clear Filter</FilterSlottedButton>
-  
+    <!-- will clear the filters only when the filters are active -->
+    <!-- onclick to clear the filter and revert to original results -->
+    <FilterSlottedButton v-if="isFilterActive" @click="clearFilter">Clear Filter</FilterSlottedButton>
     </div>
-    <div v-for="session in sessions" :key="session.id">
+    <div v-for="session in filteredSessions" :key="session.id">
     <!-- parent div to hold one entire session -->
     <div class="entire-session">
       <!-- session display info -->
@@ -23,8 +24,8 @@
 
       <!-- holds the presenter + time -->
       <div class="pres-time">
-         <p class="presenter">Presented by: <span class="span-pres">
-        {{ session.presenter }}</span></p>
+        <!-- for each presenter, using the presenter key in sessions, invoke the func thatll filter results by presenters -->
+         <p class="presenter">Presented by: <span class="span-pres" @click="filterPresenters(session.presenter)">{{ session.presenter }}</span></p>
         <p class="time"> {{ session.sDay }} at {{ TwelveHourFormat(session.sTime, session.sDay) }}</p>
       </div>
      
@@ -39,8 +40,9 @@
         <!-- tag text -->
         <p class="tag-text">Categories:</p>
 
-        <!-- buttns w all tags -->
-        <TagsSlottedButton v-for="tag in session.tags" :key="tag">{{ tag }}
+        <!-- btns w all tags -->
+        <!-- for all tagsm using the tag key in sessions, invoke func thatll filter results by tags -->
+        <TagsSlottedButton v-for="tag in session.tags" :key="tag" @click="filterTags(tag)">{{ tag }}
         </TagsSlottedButton>
       </div>
     </div>
@@ -50,19 +52,21 @@
 </template>
 
 <script setup>
+import { toHandlerKey } from 'vue';
+
 // Import the 'sessions' prop
-const { sessions } = defineProps(['sessions']);
+// const { sessions } = defineProps(['sessions']);
 </script>
 
 <!-- other script -->
 <script>
 export default {
+
   data() {
 
     return {
       //validating session data 
       session: {
-
       //start id at 0, work way down (int)
       id: 0,
 
@@ -83,9 +87,63 @@ export default {
 
       //start added as false (boolean)
       added: false,
-    }
+    },
+    
+    //empty off the bat - no filter chosen yet
+    presenterFilter: '',
+
+    //empty - no tags chosen
+    tagFilter: '',
   }
 },
+
+//begin props
+props: {
+  sessions: Array,
+},
+
+//begin computed methods
+computed: {
+
+//begin filtering content
+
+//filter through presenters
+allPresenters() {
+  //return a new session arr w only the appropriate presenters
+  return [...new Set(this.sessions.map(session => session.presenter))];
+},
+
+//filter through tags
+allTags() {
+  //return a new session arr w only the appropriate tags
+  return [...new Set(this.sessions.map(session => session.tags))];
+},
+
+//filtered sessions based on specific tags or presenters
+filteredSessions(){
+  //using filter method to filter the arr based on choices
+  return this.sessions.filter(session => {
+    //creating a const to find the right presenter
+    //using includes to make sure the sessions chosen include the right presenter
+    const rightPresenter = session.presenter.includes(this.presenterFilter) || this.presenterFilter === '';
+
+    //creating a const to find the right tags
+    //using the some to make sure the right tag shows up at least one
+    const rightTags = session.tags.some(tag => tag.includes(this.tagFilter)) || this.tagFilter === '';
+
+    //show the results if there is a match for presenter OR tags
+    return rightPresenter && rightTags;
+  });
+},
+
+//checking if any filter is active
+isFilterActive() {
+  //return the specific filter if there is a filter active
+  return this.presenterFilter !== '' || this.tagFilter !== '';
+}
+},
+
+//begin methods
   methods: {
     //twelve hour func 
     TwelveHourFormat(sTime) {
@@ -124,9 +182,38 @@ export default {
 
   //return the new time, will have AM or PM depending on what time it is
   return standardTime;
-}
+},
 
+//filtering methods
+
+//set the presenter filter, clear tag filter
+filterPresenters(presenter) {
+  //make the presenter filter match its appropriate person
+  this.presenterFilter = presenter;
+  //clear the tag filter
+  this.tagFilter = '';
+},
+
+//set the tag filter, clear presenter filter
+filterTags(tag) {
+  //make the tag filter match its appropriate tag
+  this.tagFilter = tag;
+  //clear the presenter filter
+  this.presenterFilter = '';
+},
+
+//clear both filters when btn is clicked 
+clearFilter() {
+  this.presenterFilter = '';
+  this.tagFilter = '';
 }
+  },
+
+
+  //begin emits
+  emits: {
+
+  }
 }
 </script>
 
